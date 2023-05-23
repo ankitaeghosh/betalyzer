22882cac9d81fbe0c5a304fd38e0567f6acc549d
ankitaeghosh <czarcasm2003@gmail.com>
2023-05-23 08:10:06 +0530

    updated plots

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 
 from flask import Flask, render_template, request
 from bokeh.embed import components
-from bokeh.charts import Line, Bar, Scatter, Histogram
+from bokeh.plotting import figure
 
 import betalyzer
 
@@ -25,21 +25,25 @@ def main():
 
     # build sector betas bar chart
     sector_betas = betalyzer.df_tickers.groupby('sector')['beta'].mean()
-    bk_sector_betas = Bar(sector_betas,  plot_width=550, plot_height=400, legend=None)
+    bk_sector_betas = figure(width=550, height=400)
+    bk_sector_betas.vbar(sector_betas)
     bk_sector_betas_script, bk_sector_betas_div = components(bk_sector_betas)
 
     # build market cap betas bar chart
     mktcap_betas = betalyzer.df_tickers.groupby('market_cap_decile')['beta'].mean()
-    bk_mc_betas = Bar(mktcap_betas, plot_width=550, plot_height=400, legend=None)
+    bk_mc_betas = figure(width=550, height=400)
+    bk_mc_betas.vbar(mktcap_betas)
     bk_mc_betas_script, bk_mc_betas_div = components(bk_mc_betas)
 
     # build market cap scatter plot
-    scatter = Scatter(betalyzer.df_tickers, x='market_cap_log', y='beta', plot_width=550, plot_height=400)
+    scatter = figure(x_axis_label='market_cap_log', y_axis_label='beta', width=550, height=400,)
+    scatter.circle(betalyzer.df_tickers.market_cap_log, betalyzer.df_tickers.beta, size = 18, color = "yellow")
     scatter_script, scatter_div = components(scatter)
 
     # build line plot for top three stocks
     top_tickers = betalyzer.df_tickers['ticker'].head(3)
-    bk_history = Line(betalyzer.df_betas[top_tickers], plot_width=550, plot_height=400)
+    bk_history = figure(width=550, height=400)
+    bk_history.line(betalyzer.df_betas[top_tickers])
     bk_history_script, bk_history_div = components(bk_history)
 
     return render_template('main.html', dt_tickers=betalyzer.df_tickers.to_dict(orient='records'),
@@ -51,19 +55,20 @@ def main():
 @app.route('/ticker/<ticker>')
 def ticker(ticker):
     # build line
-    line = Line(betalyzer.df_betas[ticker], plot_width=1000, plot_height=400)
+    line = figure(width=1000, height=400)
+    line.line(betalyzer.df_betas[ticker])
     bokeh_script, bokeh_div = components(line)
 
     # build scatter
-    scatter = Scatter(betalyzer.df_changes.head(betalyzer.window), x=betalyzer.market, y=ticker,
-                      plot_width=550, plot_height=400)
+    scatter = figure(width=550, height=400)
+    scatter.scatter(betalyzer.df_changes.head(betalyzer.window), x=betalyzer.market, y=ticker, size = 18, color = "yellow")
     scatter_script, scatter_div = components(scatter)
 
     # build histogram
     df_hist = betalyzer.df_changes[[ticker, 'SPY']].head(500).unstack().reset_index()
     df_hist.rename(columns={'level_0':'ticker', 0: 'change'}, inplace=True)
-    hist = Histogram(df_hist,  values='change', color='ticker', bins=20,
-              legend='top_right', plot_width=550, plot_height=400)
+    hist = figure(plot_width=550, plot_height=400)
+    hist.hplot(df_hist,  values='change', color='ticker', bins=20)
     hist_script, hist_div = components(hist)
 
     return render_template('ticker.html', ticker=ticker, bokeh_script=bokeh_script, bokeh_div=bokeh_div,
